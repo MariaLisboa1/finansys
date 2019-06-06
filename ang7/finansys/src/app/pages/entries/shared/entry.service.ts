8dd15c1a575dd652e67a602d9ab9e204374bff8e
zaza.naza95 <zaza.naza95@gmail.com>
2019-06-06 13:33:36 -0300

    adding association between Entry and Category

--- ang7/finansys/src/app/pages/entries/shared/entry.service.ts
+++ ang7/finansys/src/app/pages/entries/shared/entry.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from "rxjs/operators"
 import { Entry } from './entry.model';
+import { CategoryService } from '../../categories/shared/category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,8 @@ export class EntryService {
   private apiPath: string = "api/entries";
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private categoryService: CategoryService
   ) { }
 
   getAll(): Observable<Entry[]> {
@@ -32,18 +34,33 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handlerError),
-      map(this.jsonDataToEntry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handlerError),
+          map(this.jsonDataToEntry)
+        )
+      })
     )
+
   }
 
   update(entry: Entry): Observable<Entry> {
-  
-    return this.http.put(this.apiPath, entry).pipe(
-      catchError(this.handlerError),
-      map(() => entry)
-    )
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return this.http.put(this.apiPath, entry).pipe(
+          catchError(this.handlerError),
+          map(() => entry)
+        )
+      })
+    ) 
+   
   }
 
   delete(id: number): Observable<any> {
